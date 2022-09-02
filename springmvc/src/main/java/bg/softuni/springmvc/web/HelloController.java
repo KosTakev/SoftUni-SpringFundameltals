91362bcbbd1387fa8036ab7fb7496ab99d61f3de
KTakev <kostadin.takev@gmail.com>
2022-09-02 09:35:30 +0300

    adding attribute to hello() method

--- springmvc/src/main/java/bg/softuni/springmvc/web/HelloController.java
+++ springmvc/src/main/java/bg/softuni/springmvc/web/HelloController.java
@@ -1,13 +1,15 @@
 package bg.softuni.springmvc.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
-    @GetMapping(value = "/hello")
-    public String hello() {
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("num",3);
         return "helloWorld";
     }
 }
