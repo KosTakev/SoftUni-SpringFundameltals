e6832df8750f3c958e2f6642dc43a2dbdc3bd635
KTakev <kostadin.takev@gmail.com>
2022-10-15 11:00:44 +0300

    CrossOrigin on Kotlin

--- ThymeLeaf-pure/src/main/java/softuni/thymeleaf/Test.java
+++ ThymeLeaf-pure/src/main/java/softuni/thymeleaf/Test.java
@@ -1,2 +1,30 @@
-package softuni.thymeleaf;public class Test {
+package softuni.thymeleaf;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.StringWriter;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        TemplateEngine engine = createEngine();
+        Context context = new Context();
+        context.setVariable("name", "Pesho");
+        StringWriter stringWriter = new StringWriter();
+
+        engine.process("test.html",context, stringWriter);
+
+        System.out.println(stringWriter.toString());
+    }
+
+    private static TemplateEngine createEngine() {
+        TemplateEngine engine = new TemplateEngine();
+        engine.setTemplateResolver(new ClassLoaderTemplateResolver());
+        return engine;
+    }
 }
+
+
--- cookies/src/main/java/bg/softuni/cookies/web/SessionController.java
+++ cookies/src/main/java/bg/softuni/cookies/web/SessionController.java
@@ -1,2 +1,34 @@
-package bg.softuni.cookies.web;public class SessionController {
+package bg.softuni.cookies.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class SessionController {
+
+    private static final String LANG_SESSION_ATTRIBUTE = "lang";
+    private static final String DEFAULT_LANG = "en";
+
+    @GetMapping("/session")
+    public String session(HttpSession session,
+                          Model model) {
+
+        var sessionLang = session.getAttribute(LANG_SESSION_ATTRIBUTE);
+        model.addAttribute("lang",
+                sessionLang != null ? sessionLang : DEFAULT_LANG);
+
+        return "session";
+    }
+
+    @PostMapping("/session")
+    public String submitCookies(@RequestParam String lang, HttpSession session) {
+
+        session.setAttribute("lang", lang);
+        return "redirect:/session";
+    }
 }
